84b72554a201853f040db867180b41d3f03ccdb3
smokeyblues <aaronpennington@Aarons-Mac-mini.local>
2025-09-21 15:47:05 -0600

    Forgot to save changes on one file before last commit.

--- src/routes/(admin)/account/create_profile/+page.ts
+++ src/routes/(admin)/account/create_profile/+page.ts
@@ -6,9 +6,6 @@ export async function load({ parent }) {
 
   // They completed their profile! Redirect to "Select a Plan" screen.
   if (_hasFullProfile(data?.profile)) {
-    console.log(
-      "Redirecting to select plan you little bitch ('src/routes/(admin)/account/create_profile/+page.ts')",
-    )
     redirect(303, "/account/select_plan")
   }
 
